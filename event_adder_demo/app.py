19b2f708ca3c09060954e49dfcc49650cc897a52
Grimm1234 <111176237+Grimm1234@users.noreply.github.com>
2024-09-29 12:26:55 +0200

    Added basic fik evenet adder demo

---
+++ event_adder_demo/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+           
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        event_content = request.form['content']
+        new_event = Todo(content=event_content)
+        
+        try:
+            db.session.add(new_event)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error'
+        
+    else:
+        events = Todo.query.all()
+        return render_template("index.html", events=events) #events=events?
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    event_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(event_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that event"
+    
+@app.route('/update/<int:id>', methods={'GET', 'POST'})
+def update(id):
+    event = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        event.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issu updating your event'
+            
+    else:
+        return render_template('update.html', event=event)
+
+    
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
