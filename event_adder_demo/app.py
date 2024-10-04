87f2e25860b3634b3c37b1bb1f51e1954dccbdbc
Grimm1234 <111176237+Grimm1234@users.noreply.github.com>
2024-10-04 15:15:54 +0200

    Update_users app.py
    Added first sketch of the users function in backend

--- event_adder_demo/app.py
+++ event_adder_demo/app.py
@@ -14,7 +14,13 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
-           
+
+class Users(db.Model):  #error, need to debug
+    name = db.Column(db.String(100), nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -28,11 +34,34 @@ def index():
             return redirect('/')
         except:
             return 'There was an error'
+            
         
     else:
         events = Todo.query.all()
-        return render_template("index.html", events=events) #events=events?
+        users = Users.query.all()
+        return render_template("index.html", events=events, users=users)
     
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        users = Users.query.all()
+        user_name = request.form['user_input']
+        for user in users:
+            if user.name == user_name:
+                print("There is already a user by that name!")
+                return redirect('/')
+                
+        new_user = Users(name=user_name)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            print(new_user)
+            return redirect('/')
+        except:
+            return ("There was an error")
+    
+
 @app.route('/delete/<int:id>')
 def delete(id):
     event_to_delete = Todo.query.get_or_404(id)
