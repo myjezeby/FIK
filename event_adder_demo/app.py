c0abdaf4a65e432a0ed41892ac951614a66a8a6c
Grimm1234 <111176237+Grimm1234@users.noreply.github.com>
2024-10-06 11:04:17 +0200

    Update roles and idu api app.py

--- event_adder_demo/app.py
+++ event_adder_demo/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
+current_profile_info = None
+ROLES = ["student", "teacher", "parent"]
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +17,8 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 class Users(db.Model):  #error, need to debug
-    name = db.Column(db.String(100), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -40,30 +42,9 @@ def index():
         events = Todo.query.all()
         users = Users.query.all()
         return render_template("index.html", events=events, users=users)
-    
-@app.route('/submit/', methods=['POST', 'GET'])
-def submit():
-    if request.method == 'POST':
-        users = Users.query.all()
-        user_name = request.form['user_input']
-        for user in users:
-            if user.name == user_name:
-                print("There is already a user by that name!")
-                return redirect('/')
-                
-        new_user = Users(name=user_name)
-
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            print(new_user)
-            return redirect('/')
-        except:
-            return ("There was an error")
-    
 
 @app.route('/delete/<int:id>')
-def delete(id):
+def delete_event(id):
     event_to_delete = Todo.query.get_or_404(id)
     
     try:
@@ -84,11 +65,65 @@ def update(id):
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issu updating your event'
+            return 'There was an issue updating your event'
             
     else:
         return render_template('update.html', event=event)
 
+#   function for getting the id and name of current logged in user
+@app.route('/profile_process', methods=['POST'])
+def profile_process():
+    global current_profile_info # [name, surname, id, role]
+    data = request.json['profile_data'] # retrieve the data sent from JavaScript
+    print(data)
+    current_profile_info = [data["first_name"], data["last_name"], data["id"], data["role"]]
+    return redirect('/')
+    
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        users = Users.query.all()
+        user_name = current_profile_info[0] + " " + current_profile_info[1]
+        user_id = current_profile_info[2]
+        for user in users:
+            if user.id == user_id:
+                print("There is already a user by that name!")
+                return redirect('/')
+                
+        new_user = Users(name=user_name, id=user_id)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            print(new_user)
+            return redirect('/')
+        except:
+            return ("There was an error")    
+    
+@app.route('/delete_user/<int:id>')
+def delete_user(id):
+    if current_profile_info == None:
+        return "You have to log in!"
+    if current_profile_info[2] != id:
+        return "You can only delete yourself!"
+    
+    user_to_delete = Users.query.get_or_404(id)
+    
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that user"  
+    
+@app.route('/role_permission/', methods=['POST', 'GET'])
+def role_permission():
+    role = request.form['role_name']
+    print(role)
+    if role in ROLES:
+        if current_profile_info != None:
+            current_profile_info[3] = role
+    return redirect("/")
     
 with app.app_context():
     db.create_all()
