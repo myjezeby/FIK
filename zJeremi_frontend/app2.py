96eec2a5c995ac6fcc943b485a46dfd764f3f20a
Grimm1234 <111176237+Grimm1234@users.noreply.github.com>
2024-10-14 20:28:08 +0200

    Update redirect to landing not finished app2.py

--- zJeremi_frontend/app2.py
+++ zJeremi_frontend/app2.py
@@ -7,19 +7,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 current_profile_info = None
 ROLES = ["student", "teacher", "parent"]
+site = "login.html"
+
+
+@app.route('/landing-page/', methods=['POST', 'GET'])
+def landing_page_display():
+    global current_profile_info
+    print(current_profile_info)
+    # ADD THAT YOU NEED TO BE LOGGED IN FOR THE LANDING PAGE TO RENDER
+    return render_template("landing.html")
+
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
+    print(site)
+
     return render_template("login.html")
+
+
 
 #   function for getting the id and name of current logged in user
 @app.route('/profile_process', methods=['POST'])
 def profile_process():
     global current_profile_info # [name, surname, id, role]
+
     data = request.json['profile_data'] # retrieve the data sent from JavaScript
     print(data)
     current_profile_info = [data["first_name"], data["last_name"], data["id"], data["role"]]
-    return redirect('/')
+    global site
+    site = "landing.html"
+    return redirect("/landing-page/")
     
 
     
